6c991183bd29e0fb7d0d23f2d9ef8b3615a7ba19
phkrissanat <phkrissanath@gmail.com>
2026-09-15 09:30:43 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='section-users-v8-'+self.registration.scope;
-const ASSETS=['./','./index.html','./style.css','./app-users-v8.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'].map(p=>new URL(p,self.registration.scope).href);
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||!ASSETS.includes(e.request.url))return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();e.waitUntil(caches.open(CACHE).then(c=>c.put(e.request,copy)));}return r;}).catch(()=>caches.match(e.request)));});
